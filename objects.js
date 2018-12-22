e980eca74259ac0c67bd54f4b2947fd7b764f529
Claire Muller <clairermuller@gmail.com>
2018-12-22 22:19:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ var playlist = {
 
 const updatePlaylist = (playlist, artistName, songTitle) => {
   Object.assign({}, playlist, {artistName: songTitle})
+  return playlist
 };
 
 const removeFromPlaylist = (playlist, artistName) => {
