e57183873651d75e8d68f375f7acb1c7d09e642f
Claire Muller <clairermuller@gmail.com>
2018-12-22 22:29:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,6 +12,24 @@ updatePlaylis(playlist, 'My Bloody Valentine', 'Sometimes');
 updatePlaylis(playlist, 'My Bloody Valentine', 'Sometimes');
 
 
+/*
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
+*/
+
+
+
 const removeFromPlaylist = (playlist, artistName) => {
   delete playlist.artistName;
   return playlist;
