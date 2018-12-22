cf757b5340bba9d037043aca49dc2f78e5a80739
Claire Muller <clairermuller@gmail.com>
2018-12-22 23:13:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,11 +8,9 @@ const updatePlaylist = (playlist, artistName, songTitle) => {
   return playlist;
 };
 
-updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
 
 const removeFromPlaylist = (playlist, artistName) => {
   delete playlist['artistName'];
   return playlist;
 };
 
-removeFromPlaylist(playlist, 'Slowdive');
